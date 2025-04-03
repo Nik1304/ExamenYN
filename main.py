4d2b1dc3247f934604c91bd8cfd394bad460c360
Nik1304 <mnegocios339@gmail.com>
2025-04-03 07:30:50 -0500

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+def mostrar_menu():
+    print("\n---------- Menú Principal -----------")
+    print("1. Agregar nuevos servicios fotograficos")
+    print("2. Editar servicio fotografico")
+    print("3. Eliminar registro fotografico")
+    print("4. Salir")
+
+def main():
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            ()
+        elif opcion == "2":
+            ()
+        elif opcion == "3":
+            ()
+        elif opcion == "4":
+            print("Saliendo Saliendo...")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+if __name__ == "__main__":
+    main()
